3f64ced5428e9f5dbbad0c33aa06c4f492595853
Marcelo Laurentino <marcelolaurentino56@gmail.com>
2021-11-27 20:52:52 -0300

    Criando atendimento

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const customExpress = require('./config/customExpress')
 const conexao = require('./infraEstrutura/conexao')
+const tabelas = require('./infraEstrutura/tabelas')
 
 conexao.connect(erro => {
     if(erro){
         console.log(erro);
     }else{
         console.log('Conectado com sucesso');
+
+        tabelas.init(conexao)
+
         const app = customExpress()
 
         app.listen(3000, () => console.log('servidor rodando porta 3000'))
